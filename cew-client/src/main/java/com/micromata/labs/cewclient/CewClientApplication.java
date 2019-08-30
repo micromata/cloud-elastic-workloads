e42c06d741a6a57f4971810393ae6ff26b839dd8
Hagen Trinter <h.trinter@micromata.de>
2019-08-30 14:27:28 +0200

    Add client app via spring initializr

---
+++ cew-client/src/main/java/com/micromata/labs/cewclient/CewClientApplication.java
@@ -0,0 +1,13 @@
+package com.micromata.labs.cewclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CewClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CewClientApplication.class, args);
+	}
+
+}
---
+++ cew-client/src/test/java/com/micromata/labs/cewclient/CewClientApplicationTests.java
@@ -0,0 +1,16 @@
+package com.micromata.labs.cewclient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CewClientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
