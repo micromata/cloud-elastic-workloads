c081e1fe11090af63263431fea50d6d8d6c82695
Michael Lesniak <mlesniak@micromata.de>
2019-09-12 18:55:45 +0200

    Change response format

--- prime-factor-reference/index.js
+++ prime-factor-reference/index.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const { findPrimeFactors } = require('./prime-factors');
+const {
+  findPrimeFactors
+} = require('./prime-factors');
 
 const app = express();
 app.use(bodyParser.json());
 
 app.post('/', function (req, res) {
-  const result = req.body.input.map(input => findPrimeFactors(input));
-  res.send({ factors: result });
+  const result = req.body.input.map(function (input) {
+    return {
+      number: input,
+      factors: findPrimeFactors(input)
+    }
+  });
+  res.send(result);
 })
 
 app.listen(3000, () => {
