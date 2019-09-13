8ced3fdb598b7761962ea8afc8e2d0ce4338aba4
Michael Lesniak <mlesniak@micromata.de>
2019-09-13 06:59:09 +0200

    Add timeout

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ url=$1
 filename=numbers.txt
 
 numbers="["$(cat $filename|tr '\n' ,| rev | cut -c 2- | rev)"]"
-time http -v POST $url numbers:=$numbers
+time http --timeout 60000 -v POST $url numbers:=$numbers
